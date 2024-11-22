b48205fdfce93ed917235cf08d86e7126c17e312
Djenni23 <djenebadiallo2309@gmail.com>
2024-11-23 02:46:23 +0300

    cta-button added !

--- components/call-to-action.tsx
+++ components/call-to-action.tsx
@@ -2,8 +2,9 @@ import React from 'react'
 
 export default function CallToAction() {
   return (
-    <div>
+    <div className='text-center'>
      <h2 className='text-5xl font-bold '>Devenez le héros des affaires !</h2>
+     <button className='bg-neutral-100 font-bold rounded-full mt-7 px-10 py-4 text-xl'>Commandez maintenant</button>
     </div>
   )
 }
