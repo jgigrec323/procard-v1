a3541d9b211ca3d97145bfbfa7ff8ecfba5d92a0
Djenni23 <djenebadiallo2309@gmail.com>
2024-12-14 16:56:05 +0300

    footer done

--- components/call-to-action.tsx
+++ components/call-to-action.tsx
@@ -3,8 +3,8 @@ import React from 'react'
 export default function CallToAction() {
   return (
     <div className='text-center'>
-     <h2 className='text-5xl font-bold '>Devenez le héros des affaires !</h2>
-     <button className='bg-neutral-100 font-bold rounded-full mt-7 px-10 py-4 text-xl'>Commandez maintenant</button>
+     <h2 className='text-3xl md:text-5xl font-bold '>Devenez le héros des affaires !</h2>
+     <button className='bg-neutral-100 font-bold rounded-full mt-7 px-8 md:px-10  py-3 md:py-4 text-xl'>Commandez maintenant</button>
     </div>
   )
 }
