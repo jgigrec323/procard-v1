3f594a6616cfc1757b6b8674f4948c8b6363244e
Jean Galant Yombouno <jeangalanty@gmail.com>
2025-01-04 23:57:25 +0300

    ajout de nouveaux composants de formulaire et d'input, intégration de la validation avec Zod, et création de la page de contact

--- app/(routes)/contact/page.tsx
+++ app/(routes)/contact/page.tsx
@@ -1,5 +1,41 @@
+import ContactForm from "@/components/contact-form";
+import Billboard from "@/components/ui/billboard";
+import { Headphones, Mail } from "lucide-react";
+
 const ContactPage = () => {
-  return <div></div>;
+  return (
+    <div className="py-10 px-6 flex flex-col items-center">
+      {/* Title */}
+      <Billboard title="Nous contacter" />
+
+      {/* Main Content */}
+      <div className="mt-10 flex flex-col md:flex-row gap-8 w-full max-w-5xl">
+        {/* Contact Form */}
+        <div className="flex-1">
+          <ContactForm />
+        </div>
+
+        {/* Contact Info Cards */}
+        <div className="flex flex-col space-y-6 md:w-1/3">
+          {/* Phone Card */}
+          <div className="flex flex-col items-center space-y-4 p-6 bg-white rounded-lg border">
+            <Headphones size={40} />
+            <a href="tel:+224622461895" className="text-lg font-semibold">
+              +224 622 46 18 95
+            </a>
+          </div>
+
+          {/* Email Card */}
+          <div className="flex flex-col items-center space-y-4 p-6 bg-white rounded-lg border">
+            <Mail size={40} />
+            <a href="mailto:info@procard.com" className="text-lg font-semibold">
+              info@procard.com
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ContactPage;
---
+++ components/contact-form.tsx
@@ -0,0 +1,93 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "./ui/textarea";
+const formSchema = z.object({
+  fullname: z.string().min(2, {
+    message: "Entrez un nom valide",
+  }),
+  email: z.string().email({
+    message: "Entrez un email valide",
+  }),
+  message: z.string().min(10, {
+    message: "Entrez un message valide",
+  }),
+});
+
+export default function ContactForm() {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      fullname: "",
+      email: "",
+      message: "",
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log(values);
+  }
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <FormField
+          control={form.control}
+          name="fullname"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel className="text-lg">Nom complet</FormLabel>
+              <FormControl>
+                <Input placeholder="Entrez votre nom complet" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel className="text-lg">Email</FormLabel>
+              <FormControl>
+                <Input placeholder="Entrez votre adresse email" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="message"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel className="text-lg">Message</FormLabel>
+              <FormControl>
+                <Textarea
+                  rows={2} // Set the number of rows
+                  placeholder="Entrez votre message"
+                  {...field}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Envoyer</Button>
+      </form>
+    </Form>
+  );
+}
